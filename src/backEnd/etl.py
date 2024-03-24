d50a16f2f174c0f081f3209d19e4678e3e8ac759
Gabriel Ronchi Brigido <gabrielbrigido1@gmail.com>
2024-03-23 22:23:00 -0300

    feat(etl.py): conexao com google drive
    foi contruido uma classe para conexao de dados com o google drive usando a lib gspread para obter dados com seguraca

--- src/backEnd/etl.py
+++ src/backEnd/etl.py
@@ -1,1 +1,52 @@
 """Este módulo contém a classe de tratamento dados para o portal."""
+
+from pathlib import Path
+
+import gspread
+import pandas as pd
+from dotenv import load_dotenv
+
+# Importa as variáveis de ambiente
+load_dotenv(".env")
+
+
+class ConexaoDrive:
+    """Esssa clase instancia a parte de extração de dados do projeto."""
+
+    def __init__(self, caminhoCredenciais: Path) -> None:
+        """Instancia o ponto de entrada do projeto.
+
+        Args:
+            caminhoCredenciais: O caminho para o arquivo \
+            json que possui suas credenciais de acesso \
+            pela conta de serviço do Google
+
+        Returns:
+            conexaoGoogleDrive: Uma instância de conexão \
+            com o Google drive, podendo importar \
+            conteúdos do mesmo
+        """
+        self.caminhoCredenciaisGoogle = caminhoCredenciais
+        self.instanciaGoogle = gspread.service_account(
+            filename=self.caminhoCredenciaisGoogle
+        )
+
+    def importaPlanilhaPorAba(
+        self, idPLanilha: str, nomeAbaPlanilha: str
+    ) -> pd.DataFrame:
+        """Retorna um Dataframe com o conteúdo de uma \
+            planilha Google sheets compartilhada \
+            no Google drive.
+
+        Args:
+            idPLanilha: O id da planilha que você deseja importar
+            nomeAbaPlanilha: Nome da aba desejada da planilha
+
+        Returns:
+            dadosPLanilha: Dataframe com os dados da planilha
+        """
+        planilha = self.instanciaGoogle.open_by_key(idPLanilha)
+        aba = planilha.worksheet(nomeAbaPlanilha)
+        dados = aba.get_all_values()
+
+        return pd.DataFrame(dados[1:], columns=dados[0])
